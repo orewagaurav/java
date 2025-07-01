484e8d3b9b1d4a96a8f98fe2cd2a5b4ee61ad547
orewagaurav <gauravkumar47967@gmail.com>
2025-07-01 19:59:01 +0530

    java

--- hello.java
+++ hello.java
@@ -1,6 +1,15 @@
 public class hello{
     public static void main(String [] args){
         System.out.println("Hello Badmoose");
+        int num = 10;
+        System.out.println(num);
+        String str = "Hello Gaurav";
+        System.out.println(str);
+        int [] arr = {1,2,3,4,5,6,7};
+        System.out.println("Array Length: " + arr.length);
+        for(int i :arr){
+            System.out.println(i);
+        }
 
     }
 }
