7821328257592b9a0fd39db0b26acac8557aa726
orewagaurav <gauravkumar47967@gmail.com>
2025-07-11 20:07:51 +0530

    reverse string

--- Recursion/sum_array.java
+++ Recursion/sum_array.java
@@ -9,10 +9,18 @@ public class sum_array {
         System.arraycopy(arr, 1, arr2, 0, arr.length-1);
         return arr[0]+ sumArr(arr2);
     }
+    //reverse a string
+    String revStr(String str){
+        if(str.isEmpty()) return str;
+        return revStr(str.substring(1))+str.charAt(0);
+    }
+
     public static void main(String[] args) {
         sum_array obj = new sum_array();
         int []arr3 = {1,2,3,4,5,6,7};
         int result = obj.sumArr(arr3);
         System.out.println(result);
+
+        System.out.println(obj.revStr("Gaurav"));
     }
 }
